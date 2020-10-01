9d3c8385f42fdb1d200435edb6e54a4afba824c7
Meet191100 <meet19jeet21@gmail.com>
2020-10-01 14:49:24 +0530

    second

---
+++ main.dart
@@ -0,0 +1,25 @@
+import 'package:DevOps_Board/onBoarding.dart';
+import 'package:DevOps_Board/services/auth.dart';
+import 'package:DevOps_Board/services/wrapper.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+//import 'onBoarding.dart';
+
+void main() => runApp(MyApp(
+));
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamProvider<FirebaseUser>.value(
+        value: AuthService().user,
+        child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Wrapper(),
+      ),
+    );
+  }
+}
+
